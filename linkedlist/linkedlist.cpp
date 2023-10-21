d32dfeb70e5f58ecbb82ad4c03ac4d4f702d7113
Brian <briannguyenwg@gmail.com>
2023-10-22 02:51:03 +1100

    working on reversing a linkedlist

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -1,262 +1,312 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
-public:
-    int value;
-    Node *next;
-    Node(int value)
-    {
-        this->value = value;
-        next = nullptr;
-    }
+class Node {
+  public:
+   int value;
+   Node *next;
+   Node(int value) {
+      this->value = value;
+      next = nullptr;
+   }
 };
 
-class LinkedList
-{
-private:
-    Node *head;
-    Node *tail;
-    int length;
+class LinkedList {
+  private:
+   Node *head;
+   Node *tail;
+   int length;
 
-public:
-    LinkedList(int value)
-    {
-        Node *newNode = new Node(value);
-        head = newNode;
-        tail = newNode;
-        this->length = 1;
-    }
+  public:
+   LinkedList(int value) {
+      Node *newNode = new Node(value);
+      head = newNode;
+      tail = newNode;
+      this->length = 1;
+   }
 
-    ~LinkedList()
-    {
-        Node *temp = head;
-        while (head)
-        {
-            head = head->next;
-            delete temp;
-            temp = head;
-        }
-    }
+   ~LinkedList() {
+      Node *temp = head;
+      while (head) {
+         head = head->next;
+         delete temp;
+         temp = head;
+      }
+   }
 
-    void printList()
-    {
-        Node *temp = head;
+   void printList() {
+      Node *temp = head;
 
-        if (temp)
-        {
-            cout << temp->value;
+      if (temp) {
+         cout << temp->value;
+         temp = temp->next;
+      }
+      while (temp) {
+         cout << " - " << temp->value;
+         temp = temp->next;
+      }
+   }
+
+   void getHead() { cout << "Head: " << head->value << endl; }
+
+   void getTail() { cout << "Tail: " << tail->value << endl; }
+
+   void getLength() { cout << "Length: " << this->length << endl; }
+
+   void append(int value) {
+      // create a new node
+      Node *newNode = new Node(value);
+
+      // append a new node if the linkedlist empty
+      if (tail == nullptr && head == nullptr) {
+         head = newNode;
+         tail = newNode;
+         this->length = 1;
+      }
+
+      // append a new Node to the list if not
+      // empty
+      tail->next = newNode;
+      tail = newNode;
+      this->length += 1;
+   }
+
+   void deleteLast() {
+      // if linkedlist is empty
+      if (this->length == 0) return;
+
+      Node *pre, *temp;
+      // point pre, temp to head
+      pre = head;
+      temp = head;
+
+      // if only 1 node left in the list
+      if (this->length == 1) {
+         head = nullptr;
+         tail = nullptr;
+      } else {
+         // check temp->next != nullptr
+         while (temp->next) {
+            pre = temp;  // set temp to the previous
+                         // node from the last node
             temp = temp->next;
-        }
-        while (temp)
-        {
-            cout << " - " << temp->value;
-            temp = temp->next;
-        }
-    }
+         }
+         tail = pre;  // point pre to tail;
+         tail->next = nullptr;
+      }
+      this->length--;
+      delete temp;
+   }
 
-    void getHead()
-    {
-        cout << "Head: " << head->value << endl;
-    }
+   void prepend(int value) {
+      Node *newNode = new Node(value);
 
-    void getTail()
-    {
-        cout << "Tail: " << tail->value << endl;
-    }
+      // if list empty
+      if (this->length == 0) {
+         head = newNode;
+         tail = newNode;
+      }
 
-    void getLength()
-    {
-        cout << "Length: " << this->length << endl;
-    }
+      // there're nodes in the list
+      newNode->next = head;
+      head = newNode;
+      this->length++;
+   }
 
-    void append(int value)
-    {
-        // create a new node
-        Node *newNode = new Node(value);
+   void deleteFirst() {
+      // if there list empty
+      if (this->length == 0) return;
 
-        // append a new node if the linkedlist empty
-        if (tail == nullptr && head == nullptr)
-        {
-            head = newNode;
-            tail = newNode;
-            this->length = 1;
-        }
+      // if 1 node in the list
+      if (this->length == 1) {
+         head = nullptr;
+         tail = nullptr;
+      }
 
-        // append a new Node to the list if not empty
-        tail->next = newNode;
-        tail = newNode;
-        this->length += 1;
-    }
+      // there're nodes in the list
+      Node *temp = head;  // point temp to head
 
-    void deleteLast()
-    {
-        // if linkedlist is empty
-        if (this->length == 0)
-            return;
+      // move head to next node
+      head = head->next;
+      this->length--;
+      delete temp;  // delete temp
+   }
 
-        Node *pre, *temp;
-        // point pre, temp to head
-        pre = head;
-        temp = head;
+   Node *get(int index) {
+      // if index is out of range
+      if (index >= this->length || index < 0) {
+         return nullptr;
+      }
 
-        // if only 1 node left in the list
-        if (this->length == 1)
-        {
-            head = nullptr;
-            tail = nullptr;
-        }
-        else
-        {
-            // check temp->next != nullptr
-            while (temp->next)
-            {
-                pre = temp; // set temp to the previous node from the last node
-                temp = temp->next;
-            }
-            tail = pre; // point pre to tail;
-            tail->next = nullptr;
-        }
-        this->length--;
-        delete temp;
-    }
+      // (4)->(2)->(22)->nullptr;
+      // 0->1->2->nullptr - length = 3
 
-    void prepend(int value)
-    {
-        Node *newNode = new Node(value);
+      Node *temp = head;
 
-        // if list empty
-        if (this->length == 0)
-        {
-            head = newNode;
-            tail = newNode;
-        }
+      for (int i = 0; i < index; i++) {
+         temp = temp->next;  // move temp to next
+                             // node if index
+                             // fulfilled
+      }
 
-        // there're nodes in the list
-        newNode->next = head;
-        head = newNode;
-        this->length++;
-    }
+      // int i = 0;
+      // while (i < index)
+      // {
+      //     temp = temp->next;
+      //     i++;
+      // }
 
-    void deleteFist()
-    {
-        // if there list empty
-        if (this->length == 0)
-            return;
+      return temp;
+   }
 
-        // if 1 node in the list
-        if (this->length == 1)
-        {
-            head = nullptr;
-            tail = nullptr;
-        }
+   bool set(int index, int value) {
+      // get value from index
+      Node *temp = get(index);
+      // if nullptr returned
+      if (!temp) return false;
 
-        // there're nodes in the list
-        Node *temp = head; // point temp to head
+      // set value in temp to the new value
+      temp->value = value;
+      return true;
+   }
 
-        // move head to next node
-        head = head->next;
-        this->length--;
-        delete temp; // delete temp
-    }
+   bool insert(int index, int value) {
 
-    Node *get(int index)
-    {
-        // if index is out of range
-        if (index >= this->length || index < 0)
-        {
-            return nullptr;
-        }
+      // if index is out of range
+      if (index < 0 || index >= this->length) {
+         return false;
+      }
 
-        // (4)->(2)->(22)->nullptr;
-        // 0->1->2->nullptr - length = 3
+      // insert at the beginning - prepend
+      if (index == 0) {
+         this->prepend(value);
+         return true;
+      }
 
-        Node *temp = head;
+      // insert at the end - append
+      if (index == this->length - 1) {
+         this->append(value);
+         return true;
+      }
 
-        for (int i = 0; i < index; i++)
-        {
-            temp = temp->next; // move temp to next node if index fulfilled
-        }
+      // create a new node
+      Node *newNode = new Node(value);
+      // get the node before the inserting node
+      Node *temp = this->get(index - 1);
+      // poiting newNode to the next node
+      newNode->next = temp->next;
+      // pointing it to the newNode;
+      temp->next = newNode;
 
-        // int i = 0;
-        // while (i < index)
-        // {
-        //     temp = temp->next;
-        //     i++;
-        // }
+      // increase the length
+      this->length++;
 
-        return temp;
-    }
+      return true;
+   }
 
-    bool set(int index, int value)
-    {
-        // get value from index
-        Node *temp = get(index);
-        // if nullptr returned
-        if (!temp)
-            return false;
+   bool deleteNode(int index) {
+      // if index is out of range
+      if (index < 0 || index >= this->length) {
+         return false;
+      }
 
-        // set value in temp to the new value
-        temp->value = value;
-        return true;
-    }
+      // delete node if index at the beginning
+      if (index == 0) {
+         this->deleteFirst();
+         return true;
+      }
+
+      // delete ndoe if index at the index
+      if (index == this->length - 1) {
+         this->deleteLast();
+         return true;
+      }
+
+      // delete index within range
+      Node *pre = this->get(index - 1);
+      // Node *temp = this->get(index); // O(n)
+      Node *temp = pre->next;  // O(1)
+
+      pre->next = temp->next;
+      this->length--;
+      delete temp;
+      return true;
+   }
 };
 
-int main()
-{
-    LinkedList *myLinkedList = new LinkedList(4);
+int main() {
+   LinkedList *myLinkedList = new LinkedList(4);
 
-    // append new node
-    myLinkedList->append(22);
-    myLinkedList->append(1);
-    myLinkedList->append(34);
-    myLinkedList->append(52);
+   // append new node
+   myLinkedList->append(2);
+   myLinkedList->append(14);
+   myLinkedList->append(30);
+   myLinkedList->append(29);
+   myLinkedList->append(34);
 
-    myLinkedList->getHead();
-    myLinkedList->getTail();
-    myLinkedList->getLength();
+   myLinkedList->getHead();
+   myLinkedList->getTail();
+   myLinkedList->getLength();
 
-    myLinkedList->printList();
+   myLinkedList->printList();
 
-    // delete last node
-    cout << endl;
-    myLinkedList->deleteLast();
-    myLinkedList->deleteLast();
-    // myLinkedList->deleteLast();
-    // myLinkedList->deleteLast();
-    cout << "\nafter deleleLast(): ";
-    myLinkedList->getLength();
-    myLinkedList->printList();
+   // delete last node
+   cout << endl;
+   myLinkedList->deleteLast();
+   myLinkedList->deleteLast();
+   // myLinkedList->deleteLast();
+   // myLinkedList->deleteLast();
+   cout << "\nafter deleleLast(): ";
+   myLinkedList->getLength();
+   myLinkedList->printList();
 
-    // prepend a node
-    cout << endl;
-    myLinkedList->prepend(5);
-    myLinkedList->prepend(6);
-    cout << "after prepend: ";
-    myLinkedList->getLength();
-    myLinkedList->printList();
+   // prepend a node
+   cout << endl;
+   myLinkedList->prepend(5);
+   myLinkedList->prepend(6);
+   cout << "after prepend: ";
+   myLinkedList->getLength();
+   myLinkedList->printList();
 
-    // delete first node
-    cout << endl;
-    myLinkedList->deleteFist();
-    cout << "after deteteFist: ";
-    myLinkedList->getLength();
-    myLinkedList->printList();
+   // delete first node
+   cout << endl;
+   myLinkedList->deleteFirst();
+   cout << "after deteteFist: ";
+   myLinkedList->getLength();
+   myLinkedList->printList();
 
-    // get value using index;
-    cout << endl;
-    cout << "get index at 2:  " << myLinkedList->get(2)->value << endl;
-    cout << "get index at 0:  " << myLinkedList->get(0)->value << endl;
-    cout << "get index at 1:  " << myLinkedList->get(1)->value << endl;
-    // cout << "get index at 1:  " << myLinkedList->get(4)->value << endl;
+   // get value using index;
+   cout << endl;
+   cout << "get index at 2:  " << myLinkedList->get(2)->value << endl;
+   cout << "get index at 0:  " << myLinkedList->get(0)->value << endl;
+   cout << "get index at 1:  " << myLinkedList->get(1)->value << endl;
+   // cout << "get index at 1:  " <<
+   // myLinkedList->get(4)->value << endl;
 
-    // set value at index
-    cout << endl;
-    cout << "set 40 at index 2:  " << myLinkedList->set(2, 40) << endl;
-    cout << "set 65 at index 1:  " << myLinkedList->set(1, 65) << endl;
-    myLinkedList->getLength();
-    myLinkedList->printList();
+   // set value at index
+   cout << endl;
+   cout << "set 40 at index 2:  " << myLinkedList->set(2, 40) << endl;
+   cout << "set 65 at index 1:  " << myLinkedList->set(1, 65) << endl;
+   myLinkedList->getLength();
+   myLinkedList->printList();
 
-    delete myLinkedList;
+   // insert value at an index
+   cout << endl;
+   cout << endl;
+   cout << "insert value at an index\n";
+   myLinkedList->getLength();
+   myLinkedList->insert(1, 9);
+   myLinkedList->printList();
+
+   // delete node at an index
+   cout << endl;
+   cout << endl;
+   cout << "Delete node at an index" << endl;
+   myLinkedList->deleteNode(0);
+   myLinkedList->deleteNode(4);
+   myLinkedList->deleteNode(2);
+   myLinkedList->getLength();
+   myLinkedList->printList();
+
+   delete myLinkedList;
 }
